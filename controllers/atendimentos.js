a412caa1cee32f48c3ca8112f0e1b4de2dbf749f
Giovani <giovani.srodrigues@hotmail.com>
2021-09-30 20:12:19 -0300

    implementacao validacoes de erros

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -5,8 +5,7 @@ module.exports = app => {
     app.post('/atendimentos', (req, res) => { 
         const atendimento = req.body
 
-        Atendimento.adiciona(atendimento)
-        res.send('POST ATENDIMENTO') 
+        Atendimento.adiciona(atendimento, res)
     })
 }
 
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,19 +2,44 @@ const conexao = require('../infraestrutura/conexao')
 const moment = require('moment')
 
 class Atendimento {
-    adiciona(atendimento){
+    adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD-MM-YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO Atendimentos SET ?'
-        conexao.query(sql, atendimentoDatado, (err, resultados) => {
-            if (err) {
-                console.log(err)
-            }            
-            else{
-                console.log(resultados)
+
+        const dataValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteValido,
+                mensagem: 'Cliente deve conter no minimo 5 caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if (existemErros) {
+            res.status(400).json(erros)
+        }
+        else {
+            const atendimentoDatado = { ...atendimento, dataCriacao, data }
+            const sql = 'INSERT INTO Atendimentos SET ?'
+            conexao.query(sql, atendimentoDatado, (err, resultados) => {
+                if (err) {
+                    res.status(400).json(err)
+                }
+                else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }
     }
 }
 
