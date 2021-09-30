0f3395fa66d8ac6e08bfa3cd33703203e8adde08
Giovani <giovani.srodrigues@hotmail.com>
2021-09-30 19:52:43 -0300

    implementacao param data

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,9 +1,13 @@
 const conexao = require('../infraestrutura/conexao')
+const moment = require('moment')
 
 class Atendimento {
     adiciona(atendimento){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD-MM-YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
         const sql = 'INSERT INTO Atendimentos SET ?'
-        conexao.query(sql, atendimento, (err, resultados) => {
+        conexao.query(sql, atendimentoDatado, (err, resultados) => {
             if (err) {
                 console.log(err)
             }            
