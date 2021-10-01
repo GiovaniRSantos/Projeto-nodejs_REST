7345da922bd39afb1a0c2af5aa9f97f860ac45d3
Giovani <giovani.srodrigues@hotmail.com>
2021-10-01 12:56:30 -0300

    implementacao api externa

--- arquivos/uploadArquivos.js
+++ arquivos/uploadArquivos.js
@@ -1,10 +1,21 @@
 const fs = require('fs')
+const path = require('path')
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoValido = tiposValidos.indexOf(tipo.substring(1)) !== -1
 
-module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => 
-{
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
+    if (tipoValido) {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(false, novoCaminho))
+    }
+    else {
+        const erro = 'Tipo é invalido'
+        console.log('Tipo invalido')
+        callbackImagemCriada(erro)
+    }
 
-    fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho))
+
 }
--- models/pets.js
+++ models/pets.js
@@ -3,22 +3,26 @@ const uploadArquivo = require('../arquivos/uploadArquivos')
 
 class Pet {
     adiciona(pet, res) {
-        const query = 'INSERT INTO Pets SET ?'
+        const query = 'INSERT INTO Pets SET?'
 
-        uploadArquivo(pet.imagem, pet.nome, novoCaminho => {
-            const novoPet = { nome: pet.nome, imagem: novoCaminho }
-
-            conexao.query(query, novoPet, err => {
-                if (err) {
-                    console.log(err)
-                    res.status(400).json(err)
-                }
-                else {
-                    res.status(200).json(pet)
-                }
-            })
+        uploadArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+            if (erro) {
+                res.status(400).json({ erro })
+            } else {
+                const novoPet = { nome: pet.nome, imagem: novoCaminho }
+                conexao.query(query, novoPet, erro => {
+                    if (erro) {
+                        console.log(erro)
+                        res.status(400).json(erro)
+                    } else {
+                        res.status(200).json(pet)
+                    }
+                })
+            }
         })
     }
 }
+
+
 
 module.exports = new Pet()
